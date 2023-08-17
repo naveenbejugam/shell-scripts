dc9e8174ce8844663842946c9a1678c910e2fc13
Naveen Kumar <naveenbejugam1999@gmail.com>
2023-08-17 14:40:53 +0530

    shell-scripting

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -3,6 +3,8 @@
 echo "hello world"
 
 
-echo "Ramesh:hello"
-echo "Suresh:hai. how are u ?"
-echo "Ramesh:good"
+Person1=Ramesh
+Person2=Suresh
+echo "Person1:hello"
+echo "Person2:hai. how are u ?"
+echo "Person1:good"
