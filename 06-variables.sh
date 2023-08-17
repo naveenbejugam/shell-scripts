ddf20380907bcacf5f5d6ed4c8ecea9a03cecaaa
Naveen Kumar <naveenbejugam1999@gmail.com>
2023-08-17 16:04:23 +0530

    shell-scripting

--- 06-variables.sh
+++ 06-variables.sh
@@ -5,5 +5,9 @@ PERSONS=("Ramesh" "Suresh" "Sachin")
 # inside array we always refer values with index and it starts from 0
 
 echo "First Person: ${PERSONS[0]}"
+echo "Second Person: ${PERSONS[1]}"
+echo "Third Person: ${PERSONS[2]}"
+
+
 
 echo "All Persons: ${PERSONS[@]}"
