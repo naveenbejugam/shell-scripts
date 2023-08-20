77686e4f51bd1019732c5b228235540ab202fce2
Naveen Kumar <naveenbejugam1999@gmail.com>
2023-08-20 19:10:15 +0530

    shell-scripting

--- count.sh
+++ count.sh
@@ -6,15 +6,21 @@ read text
 
 # Counting words
 word=$(echo -n "$text" | wc -w)
-    echo word
+    
 # Counting characters
 char=$(echo -n "$text" | wc -c)
-    echo char
+   
 
 # Counting Number of white spaces (Here,specificly " ")
 # sed "s/ change this to whitespace//g"
 space=$(expr length "$text" - length `echo "$text" | sed "s/ //g"`)
-    echo space
+    
 # Counting special characters
 special=$(expr length "${text//[^\~!@#$&*()]/}")
-    echo special
+
+
+echo "Number of Words = $word"
+echo "Number of Characters = $char"
+echo "Number of White Spaces = $space"
+echo "Number of Special symbols = $special"
+    
