5807bbc17b20801ec6239179687de50af0755cdb
Naveen Kumar <naveenbejugam1999@gmail.com>
2023-08-20 17:08:24 +0530

    shell-scripting

--- reverse.sh
+++ reverse.sh
@@ -7,7 +7,9 @@ read - p "Enter string:" string
 {
 	#string
 }
-
+// looping for reversing a string
+// initialize i=len-1 for reversing a string and run till i=0
+// printing in the reverse order of the given string
 for ((i = $len - 1; i >= 0; i--))
 	do
 	// "${string:$i:1}"extract single character from string.
