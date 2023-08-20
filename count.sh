c4c32e8d099502e7172ed514cc8b909864fb3f96
Naveen Kumar <naveenbejugam1999@gmail.com>
2023-08-20 19:01:24 +0530

    shell-scripting

---
+++ count.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+echo "Enter a String"
+# Taking input from user
+read text
+
+# Counting words
+word=$(echo -n "$text" | wc -w)
+# Counting characters
+char=$(echo -n "$text" | wc -c)
+
+# Counting Number of white spaces (Here,specificly " ")
+# sed "s/ change this to whitespace//g"
+space=$(expr length "$text" - length `echo "$text" | sed "s/ //g"`)
+
+# Counting special characters
+special=$(expr length "${text//[^\~!@#$&*()]/}")
